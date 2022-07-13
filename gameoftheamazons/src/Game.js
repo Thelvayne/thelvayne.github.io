28924329fede4c99317a2b1b3e0bf1790554badb
Nicksgeschaft <ncwertetschka@gmail.com>
2022-07-13 16:06:24 +0200

    Field

--- gameoftheamazons/src/Game.js
+++ gameoftheamazons/src/Game.js
@@ -1,125 +1,140 @@
 import React from 'react'
+import Amazoness1 from './Amazoness1.jpg'
+import Amazoness2 from './Amazoness2.jpg'
 
 export default function Game() {
+  var player = 0; //player 1 = 0, player 2 = 1
+  let state = -1; //-1 free, 0 blue, 1 red, -2 arrow
+  let selected = 0; // 0 normal, 1 possible 
   
-//   var activePlayer = 0;
-// function choose(){}
-// if (activePlayer === 0){
-//  if (document.getElementsByClassName
-//  }
+
+
+          function select(row, column, state, selected){
+            if (state=== 0){if (player===0){
+              //show how you can move
+              //highlight possible Fields
+                
+            }else if(selected === 1){
+
+            }
+          }
+              
+            console.log(row + column)
+          }
+
   return (
     <div className = "board">
         
        
-        <div id="a10" className="box white"></div>
-        <div id="b10" className="box black"></div>
-        <div id="c10" className="box white"></div>
-        <div id="d10" className="box black, pieceblack"></div>
-        <div id="e10" className="box white"></div>
-        <div id="f10" className="box black"></div>
-        <div id="g10" className="box white, pieceblack"></div>
-        <div id="h10" className="box black"></div>
-        <div id="i10" className="box white"></div>
-        <div id="j10" className="box black"></div>
+        <div id="a10" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="b10" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c10" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d10" className="box black" onClick={()=>select(0,0,state,selected)}><img src={Amazoness1} alt="Amazoness1" className='Amazoness'/></div>
+        <div id="e10" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f10" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g10" className="box white" onClick={()=>select(0,0,state,selected)}><img src={Amazoness1} alt="Amazoness1" className='Amazoness'/></div>
+        <div id="h10" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i10" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j10" className="box black" onClick={()=>select(0,0,state,selected)}></div>
         
-        <div id="a9" className="box black"></div>
-        <div id="b9" className="box white"></div>
-        <div id="c9" className="box black"></div>
-        <div id="d9" className="box white"></div>
-        <div id="e9" className="box black"></div>
-        <div id="f9" className="box white"></div>
-        <div id="g9" className="box black"></div>
-        <div id="h9" className="box white"></div>
-        <div id="i9" className="box black"></div>
-        <div id="j9" className="box white"></div>
+        <div id="a9" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="b9" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c9" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d9" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="e9" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f9" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g9" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="h9" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i9" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j9" className="box white" onClick={()=>select(0,0,state,selected)}></div>
         
-        <div id="a8" className="box white"></div>
-        <div id="b8" className="box black"></div>
-        <div id="c8" className="box white"></div>
-        <div id="d8" className="box black"></div>
-        <div id="e8" className="box white"></div>
-        <div id="f8" className="box black"></div>
-        <div id="g8" className="box white"></div>
-        <div id="h8" className="box black"></div>
-        <div id="i8" className="box white"></div>
-        <div id="j8" className="box black"></div>
+        <div id="a8" className="box white" onClick={()=>select(0,0,state,selected)}><img src={Amazoness1} alt="Amazoness1" className='Amazoness'/></div>
+        <div id="b8" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c8" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d8" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="e8" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f8" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g8" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="h8" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i8" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j8" className="box black" onClick={()=>select(0,0,state,selected)}><img src={Amazoness1} alt="Amazoness1" className='Amazoness'/></div>
         
-        <div id="a7" className="box black, pieceblack"></div>
-        <div id="b7" className="box white"></div>
-        <div id="c7" className="box black"></div>
-        <div id="d7" className="box white"></div>
-        <div id="e7" className="box black"></div>
-        <div id="f7" className="box white"></div>
-        <div id="g7" className="box black"></div>
-        <div id="h7" className="box white"></div>
-        <div id="i7" className="box black"></div>
-        <div id="j7" className="box white, pieceblack">;</div>
+        <div id="a7" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="b7" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c7" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d7" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="e7" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f7" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g7" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="h7" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i7" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j7" className="box white" onClick={()=>select(0,0,state,selected)}></div>
         
-        <div id="a6" className="box white"></div>
-        <div id="b6" className="box black"></div>
-        <div id="c6" className="box white"></div>
-        <div id="d6" className="box black"></div>
-        <div id="e6" className="box white"></div>
-        <div id="f6" className="box black"></div>
-        <div id="g6" className="box white"></div>
-        <div id="h6" className="box black"></div>
-        <div id="i6" className="box white"></div>
-        <div id="j6" className="box black"></div>
+        <div id="a6" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="b6" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c6" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d6" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="e6" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f6" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g6" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="h6" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i6" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j6" className="box black" onClick={()=>select(0,0,state,selected)}></div>
         
-        <div id="a5" className="box black"></div>
-        <div id="b5" className="box white"></div>
-        <div id="c5" className="box black"></div>
-        <div id="d5" className="box white"></div>
-        <div id="e5" className="box black"></div>
-        <div id="f5" className="box white"></div>
-        <div id="g5" className="box black"></div>
-        <div id="h5" className="box white"></div>
-        <div id="i5" className="box black"></div>
-        <div id="j5" className="box white"></div>
+        <div id="a5" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="b5" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c5" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d5" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="e5" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f5" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g5" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="h5" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i5" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j5" className="box white" onClick={()=>select(0,0,state,selected)}></div>
      
-        <div id="a4" className="box white, piecewhite"></div>
-        <div id="b4" className="box black"></div>
-        <div id="c4" className="box white"></div>
-        <div id="d4" className="box black"></div>
-        <div id="e4" className="box white"></div>
-        <div id="f4" className="box black"></div>
-        <div id="g4" className="box white"></div>
-        <div id="h4" className="box black"></div>
-        <div id="i4" className="box white"></div>
-        <div id="j4" className="box black, piecewhite"></div>
+        <div id="a4" className="box white" onClick={()=>select(0,0,state,selected)}><img src={Amazoness2} alt="Amazoness2" className='Amazoness'/></div>
+        <div id="b4" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c4" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d4" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="e4" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f4" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g4" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="h4" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i4" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j4" className="box black" onClick={()=>select(0,0,state,selected)}><img src={Amazoness2} alt="Amazoness2" className='Amazoness'/></div>
      
-        <div id="a3" className="box black"></div>
-        <div id="b3" className="box white"></div>
-        <div id="c3" className="box black"></div>
-        <div id="d3" className="box white"></div>
-        <div id="e3" className="box black"></div>
-        <div id="f3" className="box white"></div>
-        <div id="g3" className="box black"></div>
-        <div id="h3" className="box white"></div>
-        <div id="i3" className="box black"></div>
-        <div id="j3" className="box white"></div>
+        <div id="a3" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="b3" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c3" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d3" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="e3" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f3" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g3" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="h3" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i3" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j3" className="box white" onClick={()=>select(0,0,state,selected)}></div>
 
-        <div id="a2" className="box white"></div>
-        <div id="b2" className="box black"></div>
-        <div id="c2" className="box white"></div>
-        <div id="d2" className="box black"></div>
-        <div id="e2" className="box white"></div>
-        <div id="f2" className="box black"></div>
-        <div id="g2" className="box white"></div>
-        <div id="h2" className="box black"></div>
-        <div id="i2" className="box white"></div>
-        <div id="j2" className="box black"></div>
+        <div id="a2" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="b2" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c2" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d2" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="e2" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f2" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g2" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="h2" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i2" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j2" className="box black" onClick={()=>select(0,0,state,selected)}></div>
    
-        <div id="a1" className="box black"></div>
-        <div id="b1" className="box white"></div>
-        <div id="c1" className="box black"></div>
-        <div id="d1" className="box white, piecewhite"></div>
-        <div id="e1" className="box black"></div>
-        <div id="f1" className="box white"></div>
-        <div id="g1" className="box black, piecewhite"></div>
-        <div id="h1" className="box white"></div>
-        <div id="i1" className="box black"></div>
-        <div id="j1" className="box white"></div>
+        <div id="a1" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="b1" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="c1" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="d1" className="box white" onClick={()=>select(0,0,state,selected)}><img src={Amazoness2} alt="Amazoness2" className='Amazoness'/></div>
+        <div id="e1" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="f1" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="g1" className="box black" onClick={()=>select(0,0,state,selected)}><img src={Amazoness2} alt="Amazoness2" className='Amazoness'/></div>
+        <div id="h1" className="box white" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="i1" className="box black" onClick={()=>select(0,0,state,selected)}></div>
+        <div id="j1" className="box white" onClick={()=>select(0,0,state,selected)}></div>
    </div>
   )
 }
