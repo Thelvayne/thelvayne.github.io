16bb31fb16a4474a034aaf7bbb8587df215f1f44
Jäckel, Philipp <philipp.jaeckel.1998@gmail.com>
2022-09-12 04:16:05 +0200

    Create CreateNewBoard.js

---
+++ gameoftheamazons/src/game/createBoard/CreateNewBoard.js
@@ -0,0 +1,8 @@
+import { newGame } from "../../communication/Communication"
+
+export const createBoard = async (rows, columns, amountAmazons) => {
+    let board = [[],];
+    // TODO: add algorithm for creating game according to user-input
+
+    return board;
+}
