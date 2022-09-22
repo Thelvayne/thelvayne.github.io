9b12391a1547c072baebd43570c5980868be4628
Jäckel, Philipp <philipp.jaeckel.1998@gmail.com>
2022-09-22 19:14:22 +0200

    Update index.js

--- gameoftheamazons/src/index.js
+++ gameoftheamazons/src/index.js
@@ -11,8 +11,8 @@ import './help/Help.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
