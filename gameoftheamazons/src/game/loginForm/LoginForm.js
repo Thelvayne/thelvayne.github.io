9baf16eb9fccc35641bad5acd45f3b739dc5e420
Nicksgeschaft <ncwertetschka@gmail.com>
2022-09-25 13:24:10 +0200

    GameLobby

--- gameoftheamazons/src/game/loginForm/LoginForm.js
+++ gameoftheamazons/src/game/loginForm/LoginForm.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function LoginForm() {
+
+
+function LoginForm({Login, error}) {
+    const [details, setDetails] = useState({name: "", password:""});
+    const submitHandler = e => {
+      e.preventDefault();
+
+      Login(details);
+  }
   return (
-    <form >
+    <form onSubmit={submitHandler}>
         <div className='form-inner'>
             <h2>Login</h2>
-          
+            {(error !== "") ? ( <div className='error'>{error}</div>): ""}
             <div className='form-group'>
                 <label htmlFor='name'>Name:</label>
-                <input type='text' name="name" id='name'></input>
+                <input type='text' name="name" id='name' onChange={e => setDetails({...details, name: e.target.value})} value={details.name}></input>
 
 
             </div>
         
             <div className='form-group'>
                 <label htmlFor='password'>Passwort:</label>
-                <input type='password' name="password" id='password'></input>
+                <input type='password' name="password" id='password' onChange={e => setDetails({...details, password: e.target.value})} value={details.password}></input>
             </div>
-            <input type='submit' value="login"></input>
+            <input type='submit' className='submit' value="login"></input>
         </div>
     </form>
   )
--- gameoftheamazons/src/gamelobby/Gamelobby.js
+++ gameoftheamazons/src/gamelobby/Gamelobby.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import {useState} from 'react';
 import LoginForm from '../game/loginForm/LoginForm';
+import { GenerateBoard } from '../game/createBoardSettings/GenerateBoard';
 
 
 
@@ -12,54 +13,86 @@ export default function Gamelobby(){
         
         navigate("../Help")
     } 
-    function CreateLobby(){
-        document.getElementById("CLobby").classList.remove("visually-hidden");
+    function CreateGame(){
+        document.getElementById("CGame").classList.remove("visually-hidden");
     }
-    function GotoLogin(){
-        navigate("../Startscreen")
-    }
+   
     const adminUser= {
+        name: "admin",
         password: "admin123"
     }
     const [user, setUser] = useState({name: ""});
     const [error, setError] = useState("");
 
+
     const Login = details => {
         console.log(details);
-    }
-    const Logout = () => 
+
+        if (details.name === adminUser.name && details.password === adminUser.password){
+
+        console.log("Logged in");
+        setUser({
+            name: details.name
+
+        });
+            }else{
+                console.log("Details do not match");
+                setError("Details do not match")
+            }
+        }
+    const Logout = () => {
+        setUser({
+            name: ""
+        })
         console.log("Logout");
+    }
+        
     return (
         <div>
         <div class ="sidenav">
         <h1>Game Of the Amazons</h1>
         {(user.name !== "") ? (
-            <h2>Welcome, <span>{user.name}</span></h2>
+            <div className='welcome'>
+                <h2>Welcome, <span>{user.name}</span></h2>
+                <button onClick={Logout}>Logout</button>
+                <button onClick={CreateGame}>Create new Game</button>
+                <button onClick={OpenRules}>Rules</button>
+            </div>
         ):(
-          <LoginForm />  
+          <LoginForm Login={Login} error={error}/>  
         )}
-        <button onClick={CreateLobby}>Create new Lobby</button>
-        <button onClick={OpenRules}>Rules</button>
-        <button onClick={GotoLogin}>Login</button>
-        <button onClick={GotoLogin}>Sign Up</button>
+        
+        
     </div>
     <div className="main">
        
-        <div className="OpenLobbies">
-            <h1>Open Lobbies</h1>
-            <button onClick={CreateLobby}>Create new Lobby</button>
-            
+        <div className="OpenGames">
+            <h1>Existing Games</h1>
+            <p>Click on game to Spectate</p>
+            {(user.name !== "") ? (
+            <button onClick={CreateGame}>Create new Game</button>
+            ):(
+                <div>
+                    <p>Login first to create a Game</p>
+                </div>
+              )}
+              <li><a href='#'>Game von Atze</a></li>
+
+              <li><a href='#'>Game von Atze2</a></li>
         </div>
 
             
 
     </div>
-    <div id="CLobby" className="visually-hidden CLobby">
-        <h1 className="Create Lobby">Create Lobby</h1>
-        <p>Lobby Name</p>
-        <input id="lobbyName" type="text"></input>
-        <button id="createLobby" onClick={CreateLobby}>Create Lobby</button>
+    <div id="CGame" className="visually-hidden CGame">
+        <h1 className="CreateGame">Create Game</h1>
+        <p>Game Name</p>
+        <input id="gameName" type="text"></input>
+        < GenerateBoard/>
+
+        <button id="createGame" className="createGame" onClick={CreateGame}>Create Game</button>
     </div>
+
     </div>
     )
 }
