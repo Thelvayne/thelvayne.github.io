833f9696fbfc6dd0df02b95eba9178989f388d7b
Jäckel, Philipp <philipp.jaeckel.1998@gmail.com>
2022-09-28 17:53:51 +0200

    Update Gamelobby.js

--- gameoftheamazons/src/gamelobby/Gamelobby.js
+++ gameoftheamazons/src/gamelobby/Gamelobby.js
@@ -6,34 +6,21 @@ import { GenerateBoard } from '../game/createBoardSettings/GenerateBoard';
 import { createPlayer, deletePlayer, getGames, getPlayers } from '../communication/Communication';
 
 export default function Gamelobby() {
-    let [searchParams] = useSearchParams();
-    
-    let userId = searchParams.get("userId");
-    if (userId === undefined || userId === null || Number.isNaN(userId)) {
-        userId = '-1'
+    var [searchParams] = useSearchParams();
+
+    const log = () => {
+        console.log("UserId: " + searchParams.get("userId"))
+        console.log("pId: " + searchParams.get("pId"))
+        console.log("opId: " + searchParams.get("opId"))
     }
+    setInterval(log, 15000);
 
-    let pId = searchParams.get("pId");
-    if (pId === undefined || pId === null || Number.isNaN(pId)) {
-        pId = '-1'
-    }
-
-    let opId = searchParams.get("opId");
-    if (opId === undefined || opId === null || Number.isNaN(opId)) {
-        opId = '-1'
-    }
-
-    setInterval(console.log("Das ist von der Gamelobby.js, um zu sehen ob es die ID von searchParams: " + pId + ", " + opId), 1000);
-
-
-
-
-    let navigate = useNavigate();
+    var navigate = useNavigate();
     function OpenRules() {
-        userId !== -1 ? opId !== -1 ? 
-        navigate("../Help/?userId=" + userId + "&pId=" + pId + "&opId=" + opId) : 
-        navigate("../Help/?userId=" + userId + "&pId=" + pId) : 
-        navigate("../Help/");
+        searchParams.get("userId") !== '-1' ? searchParams.get("opId") !== '-1' ?
+            navigate("../Help/?userId=" + searchParams.get("userId") + "&pId=" + searchParams.get("pId") + "&opId=" + searchParams.get("opId")) :
+            navigate("../Help/?userId=" + searchParams.get("userId") + "&pId=" + searchParams.get("pId")) :
+            navigate("../Help/");
     }
     function CreateGame() {
         document.getElementById("CGame").classList.remove("visually-hidden");
@@ -47,31 +34,45 @@ export default function Gamelobby() {
     const [error, setError] = useState("");
 
 
-    const Login = details => {
+    const Login = async details => {
         console.log(details);
 
-     
 
-            console.log("Logged in");
-            setUser({
-                name: details.name
-            });
-            createPlayer(details.name).then((c)=>{pId = c.id});
-        
+
+        console.log("Logged in");
+        setUser({
+            name: details.name
+        });
+        var p = await createPlayer(details.name);
+        var playerId = p.id;
+        var userId = playerId.toString();
+        if ('URLSearchParams' in window) {
+            searchParams.set("userId", userId);
+            searchParams.set("pId", userId);
+            console.log(searchParams.get("userId") + ", " + searchParams.get("pId"));
+            var newURL = window.location.pathname + '?' + searchParams.toString();
+            window.history.pushState({}, null, newURL);
+            console.log("Das ist von der Gamelobby.js, um zu sehen ob es die ID von searchParams: " +
+                searchParams.get("userId") + ", " +
+                searchParams.get("pId") + ", " +
+                searchParams.get("opId")
+            );
+        }
     }
-   const Logout = async() => {
+    const Logout = async () => {
         setUser({
             name: ""
         })
         document.getElementById("CGame").classList.add("visually-hidden");
         document.getElementById("sidebarright").classList.add("visually-hidden");
         console.log("Logout");
-        await deletePlayer(pId);
+        console.log(searchParams.get("userId"));
+        await deletePlayer(Number(searchParams.get("userId")))
     }
 
     const renderGameList = async () => {
         const allCurrentGames = await getGames();
-        console.log(await allCurrentGames);
+        // console.log(await allCurrentGames);
 
         const parent = document.getElementById("listOfGames");
         if (parent.childElementCount !== 0) {
@@ -86,7 +87,7 @@ export default function Gamelobby() {
                 // console.log(await allCurrentGames.games[ind].players);
                 const child = document.createElement('li');
                 const baby = document.createElement('a');
-                baby.href = "/Game/?id=" + allCurrentGames.games[ind].id;
+                baby.href = "/Game/?userId" + searchParams.get("userId") + "&gameId=" + allCurrentGames.games[ind].id;
                 baby.innerText = "Spiel " + allCurrentGames.games[ind].id;
                 child.appendChild(baby);
                 parent.appendChild(child);
@@ -99,7 +100,7 @@ export default function Gamelobby() {
     const renderPlayerList = async () => {
         const allCurrentPlayer = await getPlayers();
         const list = await getAllPlayersInGames()
-        console.log(await allCurrentPlayer + "||" + list);
+        // console.log(await allCurrentPlayer + "||" + list);
 
         const parent = document.getElementById("sidebarright");
         if (parent.childElementCount !== 0) {
@@ -109,18 +110,18 @@ export default function Gamelobby() {
         }
         for (const ind in allCurrentPlayer.players) {
             if (Object.hasOwnProperty.call(allCurrentPlayer.players, ind)) {
-                if (!list.includes(allCurrentPlayer.players[ind].id)){
-                const child = document.createElement('li');
-                const baby = document.createElement('a');
-                // console.log(allCurrentPlayer.players);
-                // console.log(ind);
-                // console.log(allCurrentPlayer.players[ind]);
-                baby.innerHTML = allCurrentPlayer.players[ind].name;
-                baby.id = allCurrentPlayer.players[ind].id;
-                baby.className = "clickable"
-                child.appendChild(baby);
-                child.style.cursor = "pointer";
-                parent.appendChild(child);
+                if (!list.includes(allCurrentPlayer.players[ind].id)) {
+                    const child = document.createElement('li');
+                    const baby = document.createElement('a');
+                    // console.log(allCurrentPlayer.players);
+                    // console.log(ind);
+                    // console.log(allCurrentPlayer.players[ind]);
+                    baby.innerHTML = allCurrentPlayer.players[ind].name;
+                    baby.id = allCurrentPlayer.players[ind].id;
+                    baby.className = "clickable"
+                    child.appendChild(baby);
+                    child.style.cursor = "pointer";
+                    parent.appendChild(child);
                 }
             }
         }
@@ -129,13 +130,13 @@ export default function Gamelobby() {
     const getAllPlayersInGames = async () => {
         const games = await getGames();
         var list = new Array([]);
-        console.log(await games);
+        // console.log(await games);
         for (const ind in games.games) {
-            if (Object.hasOwnProperty.call(games.games, ind)) {       
+            if (Object.hasOwnProperty.call(games.games, ind)) {
                 const playerOne = games.games[ind].players[0];
                 const playerTwo = games.games[ind].players[1];
                 list.push(playerOne);
-                list.push(playerTwo);        
+                list.push(playerTwo);
             }
         }
         return list;
@@ -144,15 +145,15 @@ export default function Gamelobby() {
     setInterval(renderPlayerList, 5000);
 
     const choseOpponent = (evt) => {
-        let opponentId = evt.target.id;
-        let opponentIdStr = opponentId.toString()
+        var opponentId = evt.target.id;
+        var opponentIdStr = opponentId.toString()
 
         if (evt.target.className.includes("clickable")) {
             if ('URLSearchParams' in window) {
                 searchParams.set("opId", opponentIdStr);
                 var newURL = window.location.pathname + '?' + searchParams.toString();
                 window.history.pushState({}, null, newURL);
-                console.log("Das ist von der Gamelobby.js, um zu sehen ob es die ID von searchParams: " + pId + ", " + opId);
+                console.log("Das ist von der Gamelobby.js, um zu sehen ob es die ID von searchParams: " + searchParams.get("pId") + ", " + searchParams.get("opId"));
             }
         }
     }
@@ -202,7 +203,7 @@ export default function Gamelobby() {
                 <input id="gameName" type="text"></input>
                 < GenerateBoard />
 
-                {/* <button id="createGame" className="createGame" onClick={CreateGame}>Create Game</button>*/ }
+                {/* <button id="createGame" className="createGame" onClick={CreateGame}>Create Game</button>*/}
             </div>
             <div id="sidebarright" className="sidebarright" onClick={choseOpponent}>
                 Choose your Opponent.
